ee93ebeb2196905f91a52c28c0a231b989b290a5
CharmingKyu <loadkrnis@gmail.com>
2021-02-11 19:45:43 +0900

    add login get response

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 var sequelize = require('./models').sequelize;
 var app = express();
 sequelize.sync();
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('/login 에 get 요청입니다.');
 });
-
-
-
-
 module.exports = router;
