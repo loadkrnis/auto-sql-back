7bd2449a1a57cbc442c70233f8cb03933c6705b4
CharmingKyu <loadkrnis@gmail.com>
2021-02-16 18:17:49 +0900

    Fix Router erd/save/:userId

--- routes/erd.js
+++ routes/erd.js
@@ -10,16 +10,33 @@ router.post('/save/:userId', function (req, res, next) {
         },
     }).then((result) => {
         if (result == null) {
+            console.error("userId:[" + req.params.userId + "] is not exist.");
             res.send("userId:[" + req.params.userId + "] is not exist.");
         }
         else {
             Erd.findOne({
                 where: {
                     user_idx: req.params.userId,
-                    database_name: req.body.database_name
+                    database_name: req.body.databaseName
                 }
             }).then((result) => {
-                res.send(result);
+                if (result == null) {
+                    console.error("databaseName:[" + req.body.databaseName + "] is not exist in userId:[" + req.params.userId + "]");
+                    res.send("databaseName:[" + req.body.databaseName + "] is not exist in userId:[" + req.params.userId + "]");
+                }
+                Erd.update({ erd_json: req.body.erdJson },
+                    {
+                        where: { user_idx: req.params.userId, database_name: req.body.databaseName }
+                    })
+                    .then((result) => {
+                        res.send(result);
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                        res.send("update fail " + err);
+                    });
+
+                //바뀐 값 보내서 확인
             }).catch((err) => {
                 console.error(err);
                 res.json(err);
