adf070920c427cee7d3d1b880d4d2097526e8391
CharmingKyu <loadkrnis@gmail.com>
2021-02-25 19:48:31 +0900

    user.js의 [DELETE]user/:hashedEmail 수정중

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../models').users;
 const Erds = require('../models').erds;
-
+const { auth, authOnlyAccessToken } = require('./authMiddleware');
 /*
 [POST] /commit/:erdName
 {
     "hashedEmail": "hashedEmail"
 }
 */
-router.post('/', function (req, res, next) {
+router.post('/', (req, res, next) => {
   let hashedEmail = req.body.hashedEmail;
   Users.create(
     {
@@ -28,5 +28,24 @@ router.post('/', function (req, res, next) {
     });
   });
 })
+
+router.delete('/:hashedEmail', authOnlyAccessToken, (req, res, next) => {
+  let hashedEmail = req.params.hashedEmail;
+  //authToken과 hashedEmail 값 비교
+  Users.destory({
+    where: { hashed_email: hashedEmail }
+  }).then((result) => {
+    res.json({
+      code: 200,
+      result: result // USERS테이블에 생성된 튜블 값
+    });
+  }).catch((err) => {
+    console.error(err);
+    res.status(400).json({
+      code: 400,
+      err
+    });
+  });
+});
 
 module.exports = router;
