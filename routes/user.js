ecade715fe4fa5de8d056e979a8beb17611bf76b
charmingKyu <loadkrnis@gmail.com>
2021-03-04 16:06:04 +0900

    Modify [DELETE]user/:hashedEmail API

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../models').users;
 const Erds = require('../models').erds;
+const ErdCommits = require('../models').erd_commits;
+const SharedUsers = require('../models').shared_users;
+const SharedErds = require('../models').shared_erds;
+const Shared = require('../models').shard;
 const { auth, authOnlyAccessToken } = require('./authMiddleware');
 /*
 [POST] /commit/:erdName
@@ -42,30 +46,50 @@ router.delete('/:hashedEmail', authOnlyAccessToken, async (req, res, next) => {
   //erds 먼저 삭제해야함 
   //shared_users도 삭제해야함
   // Erds.findAll
-  await Erds.destroy({
-    where: { user_id: hashedEmail }
-  }).catch((err) => {
-    console.error(err);
-    res.status(400).json({
-      code: 400,
-      err
-    });
-  });
 
-  Users.destroy({
-    where: { hashed_email: hashedEmail }
-  }).then((result) => {
-    res.json({
-      code: 200,
-      result: result // USERS테이블에 생성된 튜블 값
-    });
-  }).catch((err) => {
-    console.error(err);
-    res.status(400).json({
-      code: 400,
-      err
-    });
-  });
+  // user가 공유 받은 데이터 삭제
+  await SharedUsers.destroy({ where: { user_id: hashedEmail } }); 
+  
+  //user가 생성한 shared_id 저장
+  let selectShared = await Shared.findAll({
+    where: { user_id: hashedEmail }
+  }).then((share) => {
+    if (share == null) return 0;
+    let result = share.map((val) => val.id);
+    return result;
+  }); 
+  
+  //shared_erds의 erd_id 값 저장 필요
+
+  //user가 생성한ERD의 
+  await SharedErds.destroy({ where: { shared_id: selectShared } }); 
+  await SharedUsers.destroy({ where: { shared_id: selectShared } });
+  await Shared.destroy({ where: { user_id: hashedEmail } });
+
+  // await Erds.destroy({
+  //   where: { user_id: hashedEmail }
+  // }).catch((err) => {
+  //   console.error(err);
+  //   res.status(400).json({
+  //     code: 400,
+  //     err
+  //   });
+  // });
+
+  // Users.destroy({
+  //   where: { hashed_email: hashedEmail }
+  // }).then((result) => {
+  //   res.json({
+  //     code: 200,
+  //     result: result // USERS테이블에 생성된 튜블 값
+  //   });
+  // }).catch((err) => {
+  //   console.error(err);
+  //   res.status(400).json({
+  //     code: 400,
+  //     err
+  //   });
+  // });
 });
 
 module.exports = router;
