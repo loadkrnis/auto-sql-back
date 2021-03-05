04b03a0f6f58ca0d43b92434e904eaaa13fb52cc
charmingKyu <loadkrnis@gmail.com>
2021-03-05 15:21:07 +0900

    Modify [DELETE]user/:hashedEmail API in user.js

--- routes/user.js
+++ routes/user.js
@@ -42,54 +42,27 @@ router.delete('/:hashedEmail', authOnlyAccessToken, async (req, res, next) => {
       message: "get으로 보낸 hashedEmail과 토큰의 hashedEmail이 일치하지 않습니다."
     });
   }
-  //erd_commits => 
-  //erds 먼저 삭제해야함 
-  //shared_users도 삭제해야함
-  // Erds.findAll
-
-  // user가 공유 받은 데이터 삭제
-  await SharedUsers.destroy({ where: { user_id: hashedEmail } }); 
-  
   //user가 생성한 shared_id 저장
-  let selectShared = await Shared.findAll({
-    where: { user_id: hashedEmail }
-  }).then((share) => {
-    if (share == null) return 0;
-    let result = share.map((val) => val.id);
-    return result;
-  }); 
-  
-  //shared_erds의 erd_id 값 저장 필요
-
-  //user가 생성한ERD의 
-  await SharedErds.destroy({ where: { shared_id: selectShared } }); 
-  await SharedUsers.destroy({ where: { shared_id: selectShared } });
-  await Shared.destroy({ where: { user_id: hashedEmail } });
-
-  // await Erds.destroy({
-  //   where: { user_id: hashedEmail }
-  // }).catch((err) => {
-  //   console.error(err);
-  //   res.status(400).json({
-  //     code: 400,
-  //     err
-  //   });
-  // });
-
-  // Users.destroy({
-  //   where: { hashed_email: hashedEmail }
-  // }).then((result) => {
-  //   res.json({
-  //     code: 200,
-  //     result: result // USERS테이블에 생성된 튜블 값
-  //   });
-  // }).catch((err) => {
-  //   console.error(err);
-  //   res.status(400).json({
-  //     code: 400,
-  //     err
-  //   });
-  // });
+  let selectShared = await Shared.findAll({ where: { user_id: hashedEmail } })
+    .then((result) => { return result.map((val) => val.id); });
+  if (selectShared != 0) {
+    await SharedUsers.destroy({ where: { shared_id: selectShared } });
+    await SharedErds.destroy({ where: { shared_id: selectShared } });
+  }
+  await Shared.destroy({where: {user_id:hashedEmail}});
+  let selectErdIds = await Erds.findAll({ where: { user_id: hashedEmail } })
+    .then((result) => { return result.map((val) => val.id) });
+  await ErdCommits.destroy({ where: { erd_id: selectErdIds } });
+  await Erds.destroy({ where: { user_id: hashedEmail } });
+  //유저가 공유받은 erd
+  await SharedUsers.destroy({ where: { user_id: hashedEmail } }); 
+  Users.destroy({ where: { hashed_email: hashedEmail } })
+  .then((result) => {
+    res.json({
+      code: 200,
+      result: result // USERS테이블에 삭제된 튜플 갯수
+    });
+  })
 });
 
 module.exports = router;
