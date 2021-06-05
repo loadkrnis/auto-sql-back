3acb3e26e7bdfc6a54fadbbc2775c584c948759c
CharmingKyu <loadkrnis@gmail.com>
2021-06-05 16:41:41 +0000

    add: Get method invite/:email/:userId/:sharedId

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Users = require('../models').users;
+const Erds = require('../models').erds;
+const ErdCommits = require('../models').erd_commits;
+const SharedErds = require('../models').shared_erds;
+const SharedUsers = require('../models').shared_users;
+const Shared = require('../models').shared;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('invite/:email/:userId/:sharedId', function (req, res, next) {
+  const email = req.params.email;
+  const userId = req.params.userId;
+  const sharedId = req.params.sharedId;
+  SharedUsers.create({
+    user_id:userId,
+    shared_id:sharedId
+  }).then(result => {
+    res.redirect('https://autosql.ga');
+  })
+  // res.json({ email: email, userId: userId, sharedId: sharedId });
 });
 
 
