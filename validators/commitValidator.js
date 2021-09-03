ea8acc28b8bbba0cbb45540b93145cfed61a1e67
charmingkyu <loadkrnis@gmail.com>
2021-09-04 02:28:26 +0900

    refactor: 새로운 commit validator 추가

---
+++ validators/commitValidator.js
@@ -0,0 +1,26 @@
+const validator = require('./validator');
+// const { query, param, body } = require('express-validator');
+
+exports.findAll = [
+  validator
+];
+
+exports.findOne = [
+  validator
+];
+
+exports.create = [
+  validator
+];
+
+exports.update = [
+  validator
+];
+
+exports.open = [
+  validator
+];
+
+exports.remove = [
+  validator
+];
